e948f0a33fc39ba4aa4fe334c7090c9eed26c0cb
Jasper <jasperhclaessen@gmail.com>
2019-10-02 14:47:47 +0200

    Order sql

---
+++ ManiaGaming/ManiaGaming/Context/MSSQLContext/MSSQLOrderContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ManiaGaming.Models.Data;
+using ManiaGaming.Controllers;
+using System.Data;
+using ManiaGaming.Context.IContext;
+using ManiaGaming.Context.Parsers;
+using Microsoft.Extensions.Configuration;
+
+namespace ManiaGaming.Context.MSSQLContext
+{
+    public class MSSQLOrderContext : BaseMSSQLContext, IOrderContext
+    {
+        public MSSQLOrderContext(IConfiguration config) : base(config)
+        {
+        }
+
+        public List<Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Insert(Order obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Order obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
