5ea84575c163f78a5804ee439809654eb32bc1e8
Tim <timvddussen@gmail.com>
2019-10-02 09:52:11 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ ManiaGaming/ManiaGaming/Converters/ViewModelConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManiaGaming.Converters
+{
+    public class KlantViewModelConverter
+    {
+
+        //public KlantViewModel KlantToViewModel
+    }
+}
