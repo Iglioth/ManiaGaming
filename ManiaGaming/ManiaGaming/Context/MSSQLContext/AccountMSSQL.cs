3b372f7e56782ba13004ca84c4c528865a8be53e
Lars <Iglioth@gmail.com>
2019-10-02 13:36:23 +0200

    Account context updated

--- ManiaGaming/ManiaGaming/Context/MSSQLContext/AccountMSSQL.cs
+++ ManiaGaming/ManiaGaming/Context/MSSQLContext/AccountMSSQL.cs
@@ -6,24 +6,45 @@ using ManiaGaming.Models.Data;
 using System.Data.SqlClient;
 using ManiaGaming.Controllers;
 using System.Data;
+using Microsoft.Extensions.Configuration;
+using ManiaGaming.Context.IContext;
+using ManiaGaming.Context.Parsers;
 
 namespace ManiaGaming.Context.MSSQLContext
 {
-    public class AccountMSSQL
+    public class AccountMSSQL : BaseMSSQLContext, IAccountContext
     {
-        public Account getAccount(int accountID)
-        {
-            Database database = new Database("yo");
-            DataSetParser parser = new DataSetParser();
 
+        public AccountMSSQL(IConfiguration config) : base(config)
+        {
+
+        }
+
+        public List<Account> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetById(long id)
+        {
             List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
             int RowNummer = 0;
             string query = "SELECT * FROM Account WHERE accountID = @accountID";
-            parameters.Add(new KeyValuePair<string, string>("accountID", accountID.ToString()));
-            DataSet dataset = database.ExecuteSql(query, parameters);
-            Klant klant = DataSetParser.DataSetToKlant(dataset, RowNummer);
+            parameters.Add(new KeyValuePair<string, string>("accountID", id.ToString()));
+            DataSet dataset = ExecuteSql(query, parameters);
+            Account account = DataSetParser.DataSetToAccount(dataset, RowNummer);
 
-            return klant;
+            return account;
+        }
+
+        public long Insert(Account obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Account obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
