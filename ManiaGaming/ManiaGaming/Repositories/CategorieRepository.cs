74112a7bed1f4c311ae0b7c31630954ffd7a49f2
ajimale <ah_med_kaka@hotmail.com>
2019-10-04 14:48:04 +0200

    repository updated

---
+++ ManiaGaming/ManiaGaming/Repositories/CategorieRepository.cs
@@ -0,0 +1,39 @@
+﻿using ManiaGaming.Context.IContext;
+using ManiaGaming.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManiaGaming.Repositories
+{
+    public class CategorieRepository
+    {
+        protected ICategorieContext context;
+
+        public CategorieRepository()
+        {
+            this.context = context ?? throw new NullReferenceException("De CategorieContext is leeg. ");
+        }
+
+        public List<Categorie> GetAll()
+        {
+            return context.GetAll();
+        }
+        public Categorie GetById(long id)
+        {
+            return context.GetById(id);
+        }
+
+        public long Insert(Categorie obj)
+        {
+            return context.Insert(obj);
+        }
+
+        public bool Update(Categorie obj)
+        {
+            return context.Update(obj);
+        }
+
+    }
+}
