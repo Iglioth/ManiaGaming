88045826ba64b58d7f386566ab6e8807a58d0a7e
ajimale <ah_med_kaka@hotmail.com>
2019-10-09 14:38:57 +0200

    accountviewmodelconverter  and controllers

---
+++ ManiaGaming/ManiaGaming/Converters/AccountViewModelConverter.cs
@@ -0,0 +1,40 @@
+﻿using ManiaGaming.Interfaces;
+using ManiaGaming.Models;
+using ManiaGaming.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManiaGaming.Converters
+{
+    public class AccountViewModelConverter : IViewModelConverter<Account, AccountDetailViewModel>
+    {
+        public AccountDetailViewModel ModelToViewModel(Account a)
+        {
+            AccountDetailViewModel vm = new AccountDetailViewModel()
+            {
+                AccountId = a.AccountId,
+                Email = a.Email,
+                Password = a.Password,
+                Naam = a.Naam,
+                AchterNaam = a.AchterNaam
+            };
+            
+            return vm;
+        }
+
+        public Account ViewModelToModel(AccountDetailViewModel vm)
+        {
+            Account ac = new Account()
+            {
+                AccountId = vm.AccountId,
+                Email = vm.Email,
+                Password = vm.Password,
+                Naam = vm.Naam,
+                AchterNaam = vm.AchterNaam
+            };
+            return ac;
+        }
+    }
+}
