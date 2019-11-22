661ecd4d9bc5bd28ab180cbdad831fec8bca1b68
Jasper <jasperhclaessen@gmail.com>
2019-11-22 14:35:05 +0100

    winkelwagen 2

---
+++ ManiaGaming/ManiaGaming/Models/WinkelwagenDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ManiaGaming.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManiaGaming.Models
+{
+    public class WinkelwagenDetailViewModel
+    {
+        public int WinkelWagenId { get; set; }
+        public int TotalPrijs { get; set; }
+        public List<Product> producten = new List<Product>();
+        public Account account = new Account();
+    }
+}
--- ManiaGaming/ManiaGaming/Startup.cs
+++ ManiaGaming/ManiaGaming/Startup.cs
@@ -24,6 +24,10 @@ namespace ManiaGaming
 
         public IConfiguration Configuration { get; }
 
+        
+
+       
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -57,6 +61,7 @@ namespace ManiaGaming
             services.AddScoped<FiliaalRepository>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -84,6 +89,7 @@ namespace ManiaGaming
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            
         }
     }
 }
