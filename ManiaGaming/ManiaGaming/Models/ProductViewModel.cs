ee8692a70900989755ddf691452f1480d32a4a63
Jasper <jasperhclaessen@gmail.com>
2020-01-24 12:30:47 +0100

    Caroussel fixed

--- ManiaGaming/ManiaGaming/Models/ProductViewModel.cs
+++ ManiaGaming/ManiaGaming/Models/ProductViewModel.cs
@@ -7,5 +7,6 @@ namespace ManiaGaming.Models
         public List<ProductDetailViewModel> ProductDetailViewModels { get; set; } = new List<ProductDetailViewModel>();
         public List<CategorieDetailViewModel> CategorieList { get; set; }
         public List<string> SoortList { get; set; }
+        public int carousel { get; set; }
     }
 }
